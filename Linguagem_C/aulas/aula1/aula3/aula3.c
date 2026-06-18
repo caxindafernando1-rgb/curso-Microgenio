526d28cb8d3d737915dde7ecde6ba4a883f4fc1a
Fernando Caxinda <caxindafernando1@gmail.com>
2026-06-18 13:08:38 +0100

    CursoCIntroduçao

---
+++ Linguagem_C/aulas/aula1/aula3/aula3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main() {
+    
+    printf("\n************************************\n\n");
+    printf("1- Logar 2- Cadasrar  3- Imprimir\n");
+    printf("\n*************************************\n");
+
+    printf("\n  Valor retornado: %d", printf("\nOi\n"));
+
+//estes dois comandos substituem o System("pouse"):
+    printf("\n Precione qualquer tecla");
+    getchar();
+
+
+    
+
+    return 0;
+}
---
+++ Linguagem_C/aulas/aula1/aula5/numeroreal.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+   float numero;
+
+  /*ler numero real:
+   printf("Digita um numero: ");
+   scanf("%f", &numero);
+   printf("Valor da minha variavel: %.2f", numero);
+   */
+
+   /*Ler numero inteiro:
+      int valor, valor2;
+      valor = 50;
+
+      printf("Digite um numero: ");
+      scanf("%d", &valor);
+
+      printf("Digite o segundo numero: ");
+      scanf("%d", &valor2);
+
+      printf("Valor da minha variavel: %d\nSegundo valor: %d", valor, valor2);
+      */
+      /*ler um caracter:
+      char sexo = " Masculono ";
+      printf("Digide o seu sexo[M/F]");
+      scanf("%c", &sexo);
+
+      printf("Voce e do sexo: %c", sexo);
+      
+      
+      OUTRA FORMA DE LER CARACTERES:
+      char sexo;
+      printf("DIgite ser sexo: ");
+      sexo = getchar();
+
+      printf("voce e do sexo: %c", sexo);
+      */
+
+
+
+    return 0;
+}
