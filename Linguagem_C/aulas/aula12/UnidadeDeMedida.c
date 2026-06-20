dc0cac8d5e97ff1ab9d6e545a764b43cb275a91f
Fernando Caxinda <caxindafernando1@gmail.com>
2026-06-20 07:05:00 +0100

    sizeof

---
+++ Linguagem_C/aulas/aula12/UnidadeDeMedida.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (){
+   
+   int y = 3.8;
+   printf("O 'X' mede: %d bytes\n", sizeof y);
+
+ //o short é usado para poupar espaço na memoria
+   short int x = 32734;
+   printf("o numero chort ocupar: %hi byts na memoras", sizeof x);
+
+     
+
+
+
+
+
+   return 0;
+}
