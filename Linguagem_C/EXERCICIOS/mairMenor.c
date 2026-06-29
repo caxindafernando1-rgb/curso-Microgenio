783e24a88563190101ac0cb0042f6904d0fa12c2
Fernando Caxinda <caxindafernando1@gmail.com>
2026-06-29 22:31:55 +0100

    exercicio 2

--- Linguagem_C/EXERCICIOS/mairMenor.c
+++ Linguagem_C/EXERCICIOS/mairMenor.c
@@ -8,50 +8,28 @@ int main() {
     SetConsoleCP(CP_UTF8);
     setlocale(LC_ALL, ".UTF8");
 
-    float n1, n2, n3, n4, n5, p, n;
+    int n;
     printf("Digite 3 numeros para serem comparados: \n");
-    printf("N1: ");
-    scanf("%f", &n1);
-    printf("\nN2: ");
-    scanf(" %f", &n2);
-    printf("\nN3: ");
-    scanf(" %f", &n3);
-    printf("\nN4: ");
-    scanf(" %f", &n4);
-    printf("\nN5: ");
-    scanf(" %f", &n5);
+    printf("N: ");
+    scanf("%d", &n);
 
-   if (n1 < 0){
-      p += 1;
-   }
-   else
-   n +=1;
+    if (n % 2 == 0){
+      printf("%d é divisivel por 2\n", n);
+    }
+    else
+      printf("%d não é divisivel por 2\n", n);
 
-    if (n2 < 0){
-      p += 1;
-   }
-   else
-   n +=1;
+      if (n % 3 == 0){
+      printf("%d é divisivel por 3\n", n);
+    }
+    else
+      printf("%d não é divisivel por 3\n", n);
 
-    if (n3 < 0){
-      p += 1;
-   }
-   else
-   n +=1;
-
-    if (n4 < 0){
-      p += 1;
-   }
-   else
-   n +=1;
-
-    if (n5 < 0){
-      p += 1;
-   }
-   else
-   n +=1;
-
-   printf("foram digitados %f numeros negativos\n", p);
-   printf("foram digitados %f numeros positivos", n);
+      if (n % 5 == 0){
+      printf("%d é divisivel por 5\n", n);
+    }
+    else
+      printf("%d não é divisivel por 5\n");
+   
 
 }
