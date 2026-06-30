d1b370aa3814feb1f78e5d3dea3f695180ad6c3a
Fernando Caxinda <caxindafernando1@gmail.com>
2026-06-30 15:49:49 +0100

    Update mairMenor.c

--- Linguagem_C/EXERCICIOS/mairMenor.c
+++ Linguagem_C/EXERCICIOS/mairMenor.c
@@ -8,28 +8,6 @@ int main() {
     SetConsoleCP(CP_UTF8);
     setlocale(LC_ALL, ".UTF8");
 
-    int n;
-    printf("Digite 3 numeros para serem comparados: \n");
-    printf("N: ");
-    scanf("%d", &n);
 
-    if (n % 2 == 0){
-      printf("%d é divisivel por 2\n", n);
-    }
-    else
-      printf("%d não é divisivel por 2\n", n);
-
-      if (n % 3 == 0){
-      printf("%d é divisivel por 3\n", n);
-    }
-    else
-      printf("%d não é divisivel por 3\n", n);
-
-      if (n % 5 == 0){
-      printf("%d é divisivel por 5\n", n);
-    }
-    else
-      printf("%d não é divisivel por 5\n");
-   
 
 }
