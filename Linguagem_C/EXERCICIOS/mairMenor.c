18486becda1a0da29775b25f42343c141ed9dcdc
Fernando Caxinda <caxindafernando1@gmail.com>
2026-07-04 07:16:39 +0100

    Update mairMenor.c

--- Linguagem_C/EXERCICIOS/mairMenor.c
+++ Linguagem_C/EXERCICIOS/mairMenor.c
@@ -8,6 +8,25 @@ int main() {
     SetConsoleCP(CP_UTF8);
     setlocale(LC_ALL, ".UTF8");
 
+    float dol, real, n;
+    int opcoes, c;
 
+    prontf("Digita um valor para ser convertido: ");
+    scanf("%f", &n);
 
+    printf("#Para dolar digite 1\n#Para converter em euros digite 2\n");
+    scanf(" %d", &opcoes);
+
+    switch(opcoes){
+        case 1:
+          n = real;
+          c = real * dol;
+          printf("%f são %.2f dolares", n, dol);
+          break;
+        case 2:
+          n = dol;
+          c = n * real;
+          printf("%f são %.2f reais", n, real);
+          break;
 }
+return 0;
