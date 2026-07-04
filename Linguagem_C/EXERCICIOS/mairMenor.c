09edd6bb3c0353e8e8ec865d9e4911510bb0a29d
Fernando Caxinda <caxindafernando1@gmail.com>
2026-07-04 18:16:55 +0100

    conversor

--- Linguagem_C/EXERCICIOS/mairMenor.c
+++ Linguagem_C/EXERCICIOS/mairMenor.c
@@ -4,31 +4,18 @@
 
 int main() {
     
-    SetConsoleOutputCP(CP_UTF8);
-    SetConsoleCP(CP_UTF8);
-    setlocale(LC_ALL, ".UTF8");
+  SetConsoleOutputCP(CP_UTF8);
+  SetConsoleCP(CP_UTF8);
+  setlocale(LC_ALL, ".UTF8");
 
-    float dol = 1, real =  6, n;
-    int opcoes, c;
+  char letra;
+    
+  printf("Digite um valor: ");
+  scanf("%c", &letra);
+  
+  if (letra == "a" || letra "A", ler)
 
-    printf("Digita um valor para ser convertido: ");
-    scanf("%f", &n);
+    
 
-    printf("#Para dolar digite 1\n#Para converter em euros digite 2\n");
-    printf("Opção: ");
-    scanf(" %d", &opcoes);
-
-    switch(opcoes){
-        case 1:
-          n = real;
-          c = real * dol;
-          printf("%.2f reais são %.2f dolares", n, dol);
-          break;
-        case 2:
-          n = dol;
-          c = dol * real;
-          printf("%.2f dolares são %.2f reais", n, real);
-          break;
-}
 return 0;
 }
