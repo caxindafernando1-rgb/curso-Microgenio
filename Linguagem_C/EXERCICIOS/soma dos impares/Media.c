09413c0148fb1d0bf4db524343a1873658b50507
Fernando Caxinda <caxindafernando1@gmail.com>
2026-07-15 20:55:41 +0100

    soma da media

---
+++ Linguagem_C/EXERCICIOS/soma dos impares/Media.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <locale.h>
+#include <windows.h>  
+
+int main() {
+    
+  SetConsoleOutputCP(CP_UTF8);
+  SetConsoleCP(CP_UTF8);
+  setlocale(LC_ALL, ".UTF8");
+
+  
+
+
+  
+
+
+}
