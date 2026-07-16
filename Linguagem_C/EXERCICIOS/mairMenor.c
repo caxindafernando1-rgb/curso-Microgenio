5ee6d7f2a6fe15d8dd14a0cfe772d3e3c8e59266
Fernando Caxinda <caxindafernando1@gmail.com>
2026-07-16 09:42:04 +0100

    geminia

--- Linguagem_C/EXERCICIOS/mairMenor.c
+++ Linguagem_C/EXERCICIOS/mairMenor.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <locale.h>
-#include <windows.h>  
+#include <windows.h>
 
 int main() {
-    
-  SetConsoleOutputCP(CP_UTF8);
-  SetConsoleCP(CP_UTF8);
-  setlocale(LC_ALL, ".UTF8");
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    setlocale(LC_ALL, ".UTF8");
 
-  int limite, i, s;
-  printf("Digite o valor limite: ");
-  scanf("%d", &limite);
+    int n, i;
+    long long int A = 0; // Usamos long long para evitar estouro caso n seja muito grande
 
-  for ( i = 0; i <= limite; i+=1)
-  {
-    s = limite * 2 - 1;
-    printf("%d ", s);
-  }
-  
-    
-return 0;
+    // Loop de validação: garante que o usuário digite um número maior que zero
+    do {
+        printf("Digite um número inteiro maior que zero (n): ");
+        scanf("%d", &n);
+        
+        if (n <= 0) {
+            printf("Valor inválido! O número deve ser maior que zero.\n");
+        }
+    } while (n <= 0);
+
+    // Loop para somar de 1 até n
+    for (i = 1; i <= n; i++) {
+        A += i;
+    }
+
+    printf("O valor de A (soma de 1 até %d) é: %lld\n", n, A);
+
+    return 0;
 }
