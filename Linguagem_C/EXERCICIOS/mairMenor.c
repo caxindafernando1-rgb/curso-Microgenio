03e7cb7b6da1035597dbed915f9ff9d69dd77c36
Fernando Caxinda <caxindafernando1@gmail.com>
2026-07-17 18:00:17 +0100

    Update mairMenor.c

--- Linguagem_C/EXERCICIOS/mairMenor.c
+++ Linguagem_C/EXERCICIOS/mairMenor.c
@@ -7,25 +7,6 @@ int main() {
     SetConsoleCP(CP_UTF8);
     setlocale(LC_ALL, ".UTF8");
 
-    int n, i;
-    long long int A = 0; // Usamos long long para evitar estouro caso n seja muito grande
+    int i, valorInicial, valorFinal;
 
-    // Loop de validação: garante que o usuário digite um número maior que zero
-    do {
-        printf("Digite um número inteiro maior que zero (n): ");
-        scanf("%d", &n);
-        
-        if (n <= 0) {
-            printf("Valor inválido! O número deve ser maior que zero.\n");
-        }
-    } while (n <= 0);
-
-    // Loop para somar de 1 até n
-    for (i = 1; i <= n; i++) {
-        A += i;
-    }
-
-    printf("O valor de A (soma de 1 até %d) é: %lld\n", n, A);
-
-    return 0;
-}
+    for(i = 0; i ){}
