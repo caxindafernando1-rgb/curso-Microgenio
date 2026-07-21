9442308fd302402d1241c38c705ce7f08487fc36
Fernando Caxinda <caxindafernando1@gmail.com>
2026-07-21 05:47:13 +0100

    acumulando patrimonio

--- Linguagem_C/EXERCICIOS/mairMenor.c
+++ Linguagem_C/EXERCICIOS/mairMenor.c
@@ -7,6 +7,15 @@ int main() {
     SetConsoleCP(CP_UTF8);
     setlocale(LC_ALL, ".UTF8");
 
-    int i, valorInicial, valorFinal;
+    int i, valorInicial = 1, valorFinal = 1;
 
-    for(i = 0; i ){}
+    for(i = 1; i <= 29; i++ ){
+        valorInicial = valorInicial * 2;
+        valorFinal += valorInicial;
+
+    }
+
+    printf("No final de 30 dias, você terá %d, Centavos \n\n", valorFinal);
+    printf("Isso dá %.2f dolares ", valorFinal / 100.0);
+
+}
