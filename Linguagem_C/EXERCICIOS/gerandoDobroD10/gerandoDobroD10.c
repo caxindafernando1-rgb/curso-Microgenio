b92b0461b91ee68bcf094b87e7c1555a88cd3c8b
Fernando Caxinda <caxindafernando1@gmail.com>
2026-08-05 05:13:39 +0100

    gerando Dobro 1 0

--- Linguagem_C/EXERCICIOS/gerandoDobroD10/gerandoDobroD10.c
+++ Linguagem_C/EXERCICIOS/gerandoDobroD10/gerandoDobroD10.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <locale.h>
 #include <windows.h>  
-
-                                    /* Faça um programa que some o conteúdo de dois vetores de tamanho 25 e armazene o resultado 
-                                                  em um terceiro vetor. Imprima os três vetores na tela.*/
+                                                            /*) Uma pequena loja de artesanato possui apenas um vendedor e comercializa 10 tipos de objetos 
+            diferentes. O vendedor recebe um salário de R$1100,00 acrescido de 5% do valor total de suas 
+            vendas. O valor unitário dos objetos deve ser informado e armazenado em um vetor; a quantidade 
+            vendida de cada objeto deve ficar em outro vetor, mas na mesma posição. Crie um programa que 
+            receba os preços e as quantidades vendidas, armazenando-os em seus respectivos vetores. Depois, 
+            determine e mostre:*/
 
 int main() {
     
@@ -11,14 +14,13 @@ int main() {
     SetConsoleCP(CP_UTF8);
     setlocale(LC_ALL, ".UTF8");
       
-    int l, c, num1[25] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 47, 19, 283, 38, 19, 48,93, 289, 38, 28, 18, 18, 38, 49, 58, 1 }, num2[25] =  { 21, 32, 13, 44, 55, 76, 7, 8, 9, 7, 1, 53, 3, 6, 4, 3, 29, 8, 2, 8, 1, 3, 4, 5, 9}, num[25];
+    int l, c, num1[25] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
 
 
-    printf("\n\nSoma dos dois vetores:\n");
-      for ( l = 0; l < 25; l++){
-           num[l] = num1[l] + num2[l];
-           printf("|%d| %3d + %3d = %3d \n", l, num1[l], num2[l], num[l]);
+    printf("\n\nInversão do Vetor:\n");
+      for ( l = 20; l >= 0; l--){
+           printf("|%d| %3d \n", l, num1[l]);
       }
    
     
