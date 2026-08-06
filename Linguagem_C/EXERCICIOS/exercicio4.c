c3f2388189531284eba0a4aa3592569b0d5e329e
Fernando Caxinda <caxindafernando1@gmail.com>
2026-08-06 07:52:05 +0100

    soma de cada linha/coluna da matriz

--- Linguagem_C/EXERCICIOS/exercicio4.c
+++ Linguagem_C/EXERCICIOS/exercicio4.c
@@ -2,7 +2,10 @@
 #include <locale.h>
 #include <windows.h>  
 #include <time.h>  
-                      /* Faça um programa que calcule e imprima a soma de todos os elementos de uma matriz 5 x 7.*/
+                                                            /*9) Crie um programa que preencha uma matriz 5x10 com números inteiros. Em seguida faça:
+                                        a) some cada uma das linhas armazenando o resultado em um vetor;
+                                        b) some cada uma das colunas armazenando o resultado em um vetor;
+                                        c) Imprima os dois vetores identificando qual é a soma das linhas e qual é a soma das colunas*/
 
 int main() {
     
@@ -12,24 +15,50 @@ int main() {
 
     srand(time(NULL));
 
-    int i, j, soma, matriz[5][7];
+    int l, c, soma, somaLinhas[5], somaColunas[10], matriz[5][10];
 
-     for ( i = 0; i < 5; i++){
-        for ( j = 0; j < 7; j++){
-           matriz[i][j] = rand() % 100;
+     for ( l = 0; l < 5; l++){
+        for ( c = 0; c < 10; c++){
+           matriz[l][c] = rand() % 100;
         }
     }
 
+    for ( l = 0; l < 5 ; l++){
+        soma = 0;
+        for (c = 0; c < 10; c++){
+            soma += matriz[l][c];
+        }
+        somaLinhas[l] = soma;
+    }
 
-    for ( i = 0; i < 5; i++){
-        for ( j = 0; j < 7; j++){
-            soma += matriz[i][j];
-             printf("%7d ", matriz[i][j]);
+
+    for ( c = 0; c < 10; c++){
+        soma = 0;
+        for ( l = 0; l < 5; l++){
+            soma += matriz[l][c];
+        }
+        somaColunas[c] = soma;
+    }
+    
+
+    printf("\n\nMatriz: \n");
+    for ( l = 0; l < 5; l++){
+        for (c = 0; c < 10; c++){
+            printf("%3d ", matriz[l][c]);
         }
         printf("\n");
     }
     
-    printf("\n\nSoma total: %d \n\n", soma);
-    
+    printf("\n\nSoma das Linhas: \n");
+    for (l = 0; l < 5; l++){
+        printf("Linha: %3d\t Soma: %3d\n", l, somaLinhas[l]);
+    }
+
+    printf("\n\nSoma das Colunas: \n");
+    for (c = 0; c < 10; c++){
+        printf("Coluna %3d\t Soma: %3d\n", c, somaColunas[c]);
+    }
+    printf("\n\n");
+      
  return 0;
 }
