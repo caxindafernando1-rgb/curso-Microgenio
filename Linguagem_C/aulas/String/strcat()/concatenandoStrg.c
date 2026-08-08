b4811ce63600be95e85165aed130a8859116ad81
Fernando Caxinda <caxindafernando1@gmail.com>
2026-08-08 13:49:49 +0100

    comparação das strigs

--- Linguagem_C/aulas/String/strcat()/concatenandoStrg.c
+++ Linguagem_C/aulas/String/strcat()/concatenandoStrg.c
@@ -12,12 +12,18 @@ int main() {
     SetConsoleCP(CP_UTF8);
     setlocale(LC_ALL, ".UTF8");
    
-    char c1[50] = {"Oi."};
+    char c1[50] = {"ai."};
     char c2[50] = {" Como você está?"}; 
 
-    strcat(c1, c2);
+    strcat(c1, c2);//concatenar as strgs:
 
-    printf("%s ", c1);
+    printf("%s ", c1);//strs concatenadas:
+
+    //comparar strs onde:
+    //0 == strs Iguais
+    //-1 == strs1 é maior
+    //1 == strs2 é maior
+    printf("\n comparação das Strgs: %d",  strcmp(c1, c2));
 
  return 0;
 }
