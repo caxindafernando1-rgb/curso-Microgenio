050028818303eb265ace79d12766791575a97aaf
Fernando Caxinda <caxindafernando1@gmail.com>
2026-08-14 22:33:07 +0100

    sprintf//snprintf
     snprintf(str2, 10, "%d[^\n]", tam);//str2 recebe um vetor de tamanha 10, com o conteudo' "%d[^\n]" 'com tamanho tam

---
+++ Linguagem_C/aulas/Funcões/DefinindoTamanhoDstr.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <locale.h>
+#include <windows.h> 
+#include <stdlib.h> 
+#include <string.h>
+#include <ctype.h> 
+
+int main() {
+    
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    setlocale(LC_ALL, ".UTF8");
+
+
+      int tam = 0;
+      char str[10] = {"%"}, str2[10];
+
+      printf("Digite o tamanho do vetor: ");
+      scanf("%d", &tam);
+      scanf("%c");//como limpara melhor??
+ 
+     // sprintf(str2,"%d[^\n]", tam); 
+      snprintf(str2, 10, "%d[^\n]", tam);//str2 recebe um vetor de tamanha 10, com o conteudo' "%d[^\n]" 'com tamanho tam 
+      strcat(str, str2);
+
+      char palavra[tam];
+      printf("Digite uma frase: ");
+      scanf(str, palavra);
+      printf("%s\n", palavra);
+ 
+ return 0;
+}
