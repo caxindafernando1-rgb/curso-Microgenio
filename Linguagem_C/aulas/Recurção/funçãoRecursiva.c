7c069ddc91c5e885f273e8880fc6f36fa2e55942
Fernando Caxinda <caxindafernando1@gmail.com>
2026-08-17 08:07:47 +0100

    Conversor binario//time_t
    Conversor decimal para binario usando função interativa e recursiva, comparando o tempo de execução de cada operação usando a função time_t

--- Linguagem_C/aulas/Recurção/funçãoRecursiva.c
+++ Linguagem_C/aulas/Recurção/funçãoRecursiva.c
@@ -4,28 +4,46 @@
 #include <stdlib.h> 
 #include <string.h>
 #include <ctype.h>
+#include <time.h>
 
-//fatorial:
-int Fatorial(int x){
-      if(x == 0 || x == 1)
-            return 1;
-      else
-            return x * Fatorial(x - 1);
-            printf("%d", x);
-}
+
 
 //fibonacci:
-int fibonacci (int y){
+long long int fibonacciRecursivo (int y){
       if(y == 1)
             return 0;
       else{
             if(y == 2)
                   return 1;
             else{
-                  return fibonacci(y - 1) + fibonacci (y - 2);
+                  return fibonacciRecursivo(y - 1) + fibonacciRecursivo(y - 2);
             }
       }
 }
+
+
+long long int fibonacciInterativo(int x){
+
+      long long int a = 0, b = 1, c;
+      int i = 2;
+
+      if(x == 1)
+            return a;
+      else{
+            if(x == 2)
+                  return b;
+            else{
+                  while(i < x){
+                        c = a + b;
+                        a = b;
+                        b = c;
+                        i++;
+                  }
+                  return c;
+            }
+      }
+}
+
 
 int main() {
     
@@ -34,16 +52,22 @@ int main() {
     setlocale(LC_ALL, ".UTF8");
 
 
-     int n, f;
+     int f;
+     time_t tempoInicial, tempoFinal;
 
-     printf("Digite um valor para fatorizar: ");
-     scanf("%d", &n);
-
+    
      printf("Digite o termo da fibonacci: ");
      scanf(" %d", &f);
     
-     printf("\nO fatorial de %d é %d \n", n, Fatorial(n));
-     printf("O %dº termo da fibonacci é: %d\n\n", f, fibonacci(f));
+     tempoInicial = time(NULL);
+     printf("\nO %dº termo da fibonacci Interativa é: %lld\n", f,  fibonacciInterativo(f));
+     tempoFinal = time(NULL);
+     printf("O processamento da fibonacci interativa levou %.2f segundos\n\n", difftime(tempoFinal, tempoInicial));
+
+     tempoInicial = time(NULL);
+     printf("O %dº termo da fibonacci Recursiva é: %lld\n", f, fibonacciRecursivo(f));
+     tempoFinal = time(NULL);
+     printf("O processamento da fibonacci recursiva levou %.2f segundos\n\n", difftime(tempoFinal, tempoInicial));
 
 
  return 0;
