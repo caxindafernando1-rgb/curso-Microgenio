c3a0156da47754a056e9b7a2023b8b4a06dc901b
Fernando Caxinda <caxindafernando1@gmail.com>
2026-08-20 09:20:40 +0100

    struct na struct
    usei o toupper para colocar em maiuscula o sexo digitado pelo usuario

--- Linguagem_C/aulas/tipo de dados 'STRUCT'/direntesTiposDeStruct.c
+++ Linguagem_C/aulas/tipo de dados 'STRUCT'/direntesTiposDeStruct.c
@@ -5,21 +5,32 @@
 #include <string.h>
 #include <ctype.h> 
 
+
+
+typedef struct{
+
+    int dia, mes, ano;
+
+}Data;
+
 //Primeiro tipo da estrutura struct
 typedef struct {
-      int idade;
-      char nome[100];
-      char sexo;
 
+    Data Nascimento;
+    int idade;
+    char nome[100];
+    char sexo;
 
 }Pessoa1;
 
 
 //Segundo tipo de extrutura struct
 struct Pessoa2{
-      int idade;
-      char nome[100];
-      char sexo;
+
+    Data Nascimento;
+    int idade;
+    char nome[100];
+    char sexo;
 
 };
 
@@ -46,10 +57,15 @@ int main() {
     scanf(" %d", &Fernandes.idade);
     getchar();//limpar buffer do teclado
     printf("Sexo: ");
-    scanf("%c", &Fernandes.sexo); 
+    scanf("%c", &Fernandes.sexo);
+    printf("Data de nascimento dd mm aaaa: ");
+    scanf("%d%d%d", &Fernandes.Nascimento.dia, &Fernandes.Nascimento.mes, &Fernandes.Nascimento.ano); 
+
+    Fernandes.sexo = toupper(Fernandes.sexo);//Colocar o sexo 'M/F' digitado pelo teclado em Maiuscula
 
     printf("\n\nDados do João:\nNome: %s\nSexo: %c\nIdade: %d\n\n", Joao.nome, Joao.sexo, Joao.idade);
-    printf("\n\nDados do Fernandes:\nNome: %s\nIdade: %d\nSexo: %c\n\n", Fernandes.nome, Fernandes.idade, Fernandes.sexo);
+    printf("\n\nDados do Fernandes:\nNome: %s\nIdade: %d\nSexo: %c", Fernandes.nome, Fernandes.idade, Fernandes.sexo);
+    printf("\nData de Nascimento: %d/%d/%d.\n\n\n", Fernandes.Nascimento.dia, Fernandes.Nascimento.mes, Fernandes.Nascimento.ano); 
 
  return 0;
 }
