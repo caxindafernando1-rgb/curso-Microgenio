39e0666f353a75f348fc207e90fef318e2a953ab
Fernando Caxinda <caxindafernando1@gmail.com>
2026-08-20 16:28:31 +0100

    rampa de asteristico

--- Linguagem_C/aulas/aula 62/RampaDeAst.c
+++ Linguagem_C/aulas/aula 62/RampaDeAst.c
@@ -14,6 +14,13 @@ int main() {
     scanf("%d", &n);
 
     for(i = 1; i <= n; i++){
-      printf("%d "a);
+      for(j = n - 1; j >= 1; j--)//se trocar '1' por 'i' inverte o sentido de descida da rampa de asteristico 
+        printf(" ");
+      for(j = 1; j <= i; j++)
+        printf("*");
+      printf("\n");
     }
+
+
+return 0;
 }
