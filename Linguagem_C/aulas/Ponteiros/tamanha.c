5f2cbf69e0e37a1cde6feaee200b37ee83a783a8
Fernando Caxinda <caxindafernando1@gmail.com>
2026-08-24 19:49:16 +0100

    struct de ponteiro
    manipulamos usando '->'

--- Linguagem_C/aulas/Ponteiros/tamanha.c
+++ Linguagem_C/aulas/Ponteiros/tamanha.c
@@ -2,26 +2,21 @@
 #include <locale.h>
 #include <windows.h> 
 #include <string.h> 
-#include <time.h> 
+#include <time.h>
 
-int tam = 10;
+typedef struct{
 
-void preencherStr(int vet[], int tamanho){
+    int dia, mes, ano;
 
-    srand(time(NULL));
+} Data;
 
-    for (int i = 0; i < tamanho; i++){  
-        *(vet + i) = 1 + rand() % 100; 
-    }
+
+void imprimir(Data *X){
+
+    printf("\n%d/%02d/%d ", X->dia, X->mes, X->ano);
+
 }
 
-
-void imprimir(int vet[], int tamanho){
-
-    for (int i = 0; i < tamanho; i++){
-        printf("%d ", &vet[i]);
-    }
-}
 
 int main() {
    
@@ -29,10 +24,21 @@ int main() {
     SetConsoleCP(CP_UTF8);
     setlocale(LC_ALL, ".UTF8");
 
-    int vetor[tam];
+   
 
-    preencherStr(vetor, tam);
-    imprimir(vetor, tam);
+    Data data;
+    Data *P;
+    
+    P = &data;
+    data.dia = 20;
+    data.mes = 07;
+    data.ano = 2000;
+
+    printf("\n%d/%02d/%d \n",data.dia, data.mes, data.ano);
+    printf(" \n%d // %d", &P, &data);
+    
+    imprimir(P);
+    
    
 
     return 0;
