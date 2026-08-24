f2ecba446da2ea1bb9e3495c98d5fab1d1da73fb
Fernando Caxinda <caxindafernando1@gmail.com>
2026-08-24 15:50:31 +0100

    multiplicar vetorr

--- Linguagem_C/aulas/Ponteiros/tamanha.c
+++ Linguagem_C/aulas/Ponteiros/tamanha.c
@@ -2,22 +2,41 @@
 #include <locale.h>
 #include <windows.h> 
 
+int tam = 10;
+
+void testar(int vet[], int tam){
+    for (int i = 0; i < tam; i++){
+        vet[i] = vet[i] * 2;
+    }
+    printf("\n\n");
+}
+
+
+void imprimir(int vet[], int tam){
+      for (int i = 0; i < tam; i++){
+        printf("%d ", vet[i]);
+    }
+    printf("\n");
+}
+
 int main() {
    
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
     setlocale(LC_ALL, ".UTF8");
 
-       
-   char *p;
-   int *p2;
-   float *p3;
+    
+    int vet[10] = {12, 4, 16, 32, 7, 5, 17, 56, 45, 20};
 
+    imprimir(vet, 10);
+    testar(vet, 10);
+    imprimir(vet, 10);
 
-   printf("endereço da variavel p: %d\n", sizeof(p));
-    printf("endereço da variavel p: %d\n", sizeof(p2));
-     printf("endereço da variavel p: %d\n", sizeof(p3));
+  
+
  
+       
+   
 
     return 0;
 }
