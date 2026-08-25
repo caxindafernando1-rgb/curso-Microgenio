c97b7d67ad68b898c4add705d1d82516e23b7fdc
Fernando Caxinda <caxindafernando1@gmail.com>
2026-08-25 08:20:10 +0100

    Mairo e Menor  Recursivo

--- Linguagem_C/EXERCICIOS/Ponteiros/VetorParaCalcularMaiorEMenor.c
+++ Linguagem_C/EXERCICIOS/Ponteiros/VetorParaCalcularMaiorEMenor.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <locale.h>
 #include <windows.h>  
-#include <string.h> 
+#include <string.h>
+
+
+
+
+void MaiorMenor(int *vet, int tam, int *maior, int *menor){
+
+    *maior = *vet;
+    *menor = *vet;
+
+   for (int i = 0; i < tam; i++){
+        if(*(vet + i) > *maior )
+            *maior = *(vet + i);
+        if(  *(vet + i) < *menor)
+            *menor = *(vet + i);
+    }
+}
 
 
 int main() {
@@ -10,23 +26,17 @@ int main() {
     SetConsoleCP(CP_UTF8);
     setlocale(LC_ALL, ".UTF8");
 
+    int vet[] = {1, 3, 4, 5, 6, 7, 8, 15, 10}; 
+    int maior, menor; 
+    int tam = sizeof(vet) / sizeof(vet[0]); // Calcula o tamanho do vetor e atribui a variavel tam;
 
-   int vet[] = {1, 3, 4, 5, 6, 7, 8, 15, 10}; 
-   int i, maior = 0, menor = 99;
-   int tam = sizeof(vet) / sizeof(vet[0]); // Calcula o tamanho do vetor e atribui a variavel tam;
-
-   for ( i = 0; i < tam; i++){
-    if(vet[i] > maior)
-        maior = vet[i];
-   }
-    for ( i = 0; i < tam; i++){
-    if(vet[i]  < menor)
-        menor = vet[i];}
+   
+    MaiorMenor(vet, tam,  &maior,  &menor);
 
     printf("valores do vetor: ");     
-    for ( i = 0; i < tam; i++){
+    for (int i = 0; i < tam; i++){
         printf("%d ", vet[i]);}
-   
+       
     printf("\nMaior: %d\n", maior);
     printf("Menor: %d", menor);
    
