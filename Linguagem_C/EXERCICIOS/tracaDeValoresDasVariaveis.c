97313ec89af9c97d0d8b364a18b5c3d347391f15
Fernando Caxinda <caxindafernando1@gmail.com>
2026-08-25 14:50:33 +0100

    Função que copia caracteres

--- Linguagem_C/EXERCICIOS/tracaDeValoresDasVariaveis.c
+++ Linguagem_C/EXERCICIOS/tracaDeValoresDasVariaveis.c
@@ -5,12 +5,20 @@
 
 
 /*
-                                                                Considere a seguinte declaração: int A, *B, **C, ***D
-                                                                Escreva um programa que leia a variável A e calcule e exiba o dobro, o triplo e o quádruplo desse
-                                                                valor utilizando apenas os ponteiros B, C e D. O ponteiro B deve ser usada para calcular o dobro, C
-                                                                o triplo e D o quádruplo.
+                                                               Função que copia caracteres
 */
+int minhaCopia(char *origem, char *destino){
 
+    int i = 0;
+
+    while (*(origem + i) != '\0'){
+        *(destino + i) = *(origem + i);
+        i++;
+    }
+    *(destino + i) = '\0';
+    return i;
+
+}
    
 int main() {
     
@@ -18,18 +26,10 @@ int main() {
     SetConsoleCP(CP_UTF8);
     setlocale(LC_ALL, ".UTF8");
 
-    int a, *b, **c, ***d;
+    char destino[30] = " ", origem[30] = "What tha hell!";
 
-    printf("Digite um valor: ");
-    scanf("%d", &a);
-    
-    b = &a;
-    c = &b;
-    d = &c;
-
-    printf("o dobro de: %d é: %d\n", a, 2 * *b);
-    printf("o triplo de: %d é: %d\n", *b, 3 * **c);
-    printf("o quadruplo de: %d é: %d\n", **c, 4 * ***d);
+    printf("\n\ntamnho: %d\n", minhaCopia(origem, destino));
+    printf("Destino: %s\n\n", destino);
 
  return 0;
 }
