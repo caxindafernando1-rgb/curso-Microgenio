983b5b3deeb0b6f5c58940f6e3fb27ce732767c7
Fernando Caxinda <caxindafernando1@gmail.com>
2026-09-03 19:21:37 +0100

    agenda de aniversarios

---
+++ Linguagem_C/PROJETOS/agende de aniversarios/agendaDeAniversarios.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <locale.h>
+#include <windows.h>  
+#include <string.h>
+#include <time.h> 
+
+
+/*
+                                                                 LESTA DE DASTAS DE ANIVERSARIO <>
+*/
+typedef struct{
+    char nome[100];
+    int dia, mes, ano;
+}Contacto;
+
+
+void imprimir(Contacto **c, int quant){
+    printf("\n\t== Lista de contactos ==\n");
+    printf("------------------------------------------\n");
+    for (int i = 0; i < quant; i++){
+        printf("%d = %02d/%02d/%4d -- %s\n", i+1, c[i]->dia, c[i]->mes, c[i]->ano, c[i]->nome);
+    }
+    printf("------------------------------------------\n");
+}
+
+
+int cadastrar_contacto(Contacto **c, int quant, int tam){
+    if(quant < tam){
+        Contacto *novo = malloc(sizeof(Contacto));
+        printf("Nome: ");
+        scanf(" %100[^\n]", novo->nome);
+        printf("dia: ");
+        scanf("%d", &novo->dia);
+        getchar();
+        printf("Mês: ");
+        scanf("%d", &novo->mes);
+        getchar();
+        printf("Ano: ");
+        scanf("%d", &novo->ano);
+        c[quant] = novo;
+        printf("\nContacto cadastrado com cucesso!!\n");
+        return 1;
+    }
+    else{
+        printf("Vetor sem espaço vazio!");
+        return 0;
+    }
+}
+
+
+void alterar_contacto(Contacto **c, int quant){
+    int id;
+
+    imprimir(c, quant);
+
+    printf("Digite o ID do contacto a ser alterado: ");
+    scanf("%d", &id);
+    getchar();
+    id--;
+
+    if (id >= 0 && id < quant){
+        Contacto *novo = malloc(sizeof(Contacto));
+        printf("Nome: ");
+        scanf("%100[^\n]", novo->nome);
+        printf("dia: ");
+        scanf("%d", &novo->dia);
+        getchar();
+        printf("Mês: ");
+        scanf("%d", &novo->mes);
+        getchar();
+        printf("Ano: ");
+        scanf("%d", &novo->ano);
+        c[quant] = novo;
+    }
+    else
+        printf("ERRO!\nID invalido");
+}
+
+
+int main() {
+
+  SetConsoleOutputCP(CP_UTF8);
+  SetConsoleCP(CP_UTF8);
+  setlocale(LC_ALL, ".UTF8");
+
+  Contacto *agenda[50];
+  int opcao, tam = 50, quant = 0;
+
+  do{
+    printf("1 - Cadastrar\n2 - Alterar Cadastro\n3 - Imprimir Cadastro\n4 - Salvar\n5 - Ler Arquivo\n\n");
+    printf("Opção -- ");
+    scanf("%d", &opcao);
+    getchar();
+
+    switch (opcao){
+    case 1:
+        quant += cadastrar_contacto(agenda, quant, tam);
+        break;
+    case 2:
+        alterar_contacto(agenda, quant);
+        break;
+    case 3:
+        imprimir(agenda, quant);
+        break;
+    default:
+        if (opcao != 0)
+            printf("Opção invalida!\n\n");
+        
+        break;
+    }
+  }while (opcao != 0);
+ 
+
+   return 0;
+}
